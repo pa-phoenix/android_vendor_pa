ReloadedOS/vendor_reloaded

fonts: Import oneplus slate fonts
 * from hotdog-user-10-QKQ1.190716.003-2101212101-release-keys

configs/common.mk
@@ -195,6 +195,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Fonts
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/wave/prebuilt/fonts/,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    $(call find-copy-subdir-files,*,vendor/wave/prebuilt/fonts-system/,$(TARGET_COPY_OUT_SYSTEM)/fonts) \
     vendor/wave/prebuilt/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
 PRODUCT_PACKAGES += \

pa-phoenix/android_vendor_pa

config/common.mk
@@ -46,6 +46,7 @@ TARGET_FS_CONFIG_GEN += vendor/pa/config/config.fs
 # Fonts
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/pa/prebuilt/fonts/,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    $(call find-copy-subdir-files,*,vendor/pa/prebuilt/fonts-system/,$(TARGET_COPY_OUT_SYSTEM)/fonts) \
     vendor/pa/prebuilt/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
 # GMS